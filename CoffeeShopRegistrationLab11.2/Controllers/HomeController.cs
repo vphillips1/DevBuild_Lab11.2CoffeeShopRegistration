b1b2fc8e143543dc9be3cdeae3848788591cb350
Phillips <verkeiac@yahoo.com>
2021-05-11 17:34:22 -0400

    first commit to lab 11.2

---
+++ CoffeeShopRegistrationLab11.2/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using CoffeeShopRegistrationLab11._2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShopRegistrationLab11._2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RegistrationForm()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegistrationResponse(string firstname, string lastname, string email, int phonenumber, string password )
+        {
+            if(password.Length < 8 )
+
+            {
+
+                return Content("Sorry, password must be at least 8 characters");
+            }
+            ViewData["First Name"] = firstname;
+            ViewData["Last Name"] = lastname;
+            ViewData["Email"] = email;
+            ViewData["Phone Number"] = phonenumber;
+            ViewData["Password"] = password;
+            return View();
+        }
+       
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoffeeShopRegistrationLab11.2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoffeeShopRegistrationLab11._2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
